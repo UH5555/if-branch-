855eff80f70bd284d2da72eb4825b92c4d58e880
UH5555 <zhuhao162@163.com>
2022-01-07 19:45:24 +0800

    +switch

--- test_0106/test.c
+++ test_0106/test.c
@@ -1,14 +1,73 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+
 int main()
 {
-	int num = 1;
-	int a = num % 2;
-	if (1 == a)
-		printf("奇数\n");
+	int day = 0;
+	scanf("%d\n", &day);
+	switch (day)
+	{
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+		printf("工作日\n");
+		break;
+	case 6:
+	case 7:
+		printf("休息日\n");
+		break;
+	default:
+		printf("输入错误\n");//1-7以外的输出。
+		break;
+	}
+	//case 1:
+	//	printf(" 星期一\n");
+	//	break;
+	//case 2:
+	//	printf(" 星期二\n");
+	//	break;
+	//case 3:
+	//	printf(" 星期三\n");
+	//	break;
+	//case 4:
+	//	printf(" 星期四\n");
+	//	break;
+	//case 5:
+	//	printf(" 星期五\n");
+	//	break;
+	//case 6:
+	//	printf(" 星期六\n");
+	//	break;
+	//case 7:
+	//	printf(" 星期天\n");
+	//	break;
+	//}
 	return 0;
 }
+
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//		if (i % 2 == 1)
+//			printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 1;
+//	int a = num % 2;
+//	if (1 == a)
+//		printf("奇数\n");
+//	return 0;
+//}
 //int main()
 //{
 //	int num = 4;
